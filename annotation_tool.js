e1f84579c2bd06155733c7b2be033d8773d88df7
Mazen Hassan <60842042+mhassan20328@users.noreply.github.com>
2023-10-31 17:32:46 +0200

    Implemented Firebase Login
    Moved from hard-coded login to using FirebaseAuth to log users in. Linked log-in with annotation tool.

---
+++ annotation_tool.js
@@ -0,0 +1,163 @@
+// Description: JavaScript code for the annotation tool
+// Decleration of varaibles
+// Get the canvas and context
+let canvas = document.getElementById('annotation-canvas');
+let ctx = canvas.getContext('2d');
+// Image array to store the images
+let images = [];
+// image index to iterate through the images
+let currentImageIndex = 0;
+// Annotations array to store the annotations
+let annotations = [];
+// Current annotations array to store the current annotations
+let currentAnnotations = [];
+// jszip object to create zip file
+let zip = new JSZip();
+
+document.getElementById('image-upload').addEventListener('change', (event) => {
+  const files = event.target.files;
+  images = [];
+  annotations = [];
+  currentAnnotations = [];
+  currentImageIndex = 0;
+  zip = new JSZip();
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const image = new Image();
+      image.src = e.target.result;
+      image.onload = function () {
+        images.push(image);
+        annotations.push([]);
+        if (images.length === 1) {
+          setCurrentImage(0);
+        }
+      };
+    };
+
+    reader.readAsDataURL(file);
+  }
+});
+
+canvas.addEventListener('mousedown', (event) => {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  currentAnnotations.push({
+    x: x / canvas.width,
+    y: y / canvas.height,
+    width: 0,
+    height: 0
+  });
+
+  console.log(`Annotation X: ${x / canvas.width}, Y: ${y / canvas.height}`);
+
+  canvas.addEventListener('mousemove', onMouseMove);
+  canvas.addEventListener('mouseup', onMouseUp);
+});
+
+function onMouseMove(event) {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  const currentAnnotation = currentAnnotations[currentAnnotations.length - 1];
+  currentAnnotation.width = (x - currentAnnotation.x * canvas.width) / canvas.width;
+  currentAnnotation.height = (y - currentAnnotation.y * canvas.height) / canvas.height;
+
+  drawAnnotations();
+}
+
+function onMouseUp() {
+  canvas.removeEventListener('mousemove', onMouseMove);
+  canvas.removeEventListener('mouseup', onMouseUp);
+}
+
+function setCurrentImage(index) {
+  currentImageIndex = index;
+  currentAnnotations = annotations[currentImageIndex] || [];
+  drawAnnotations();
+}
+
+function drawAnnotations() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (images.length > 0) {
+    ctx.drawImage(images[currentImageIndex], 0, 0, canvas.width, canvas.height);
+  }
+
+  for (let i = 0; i < currentAnnotations.length; i++) {
+    const annotation = currentAnnotations[i];
+    const x = annotation.x * canvas.width;
+    const y = annotation.y * canvas.height;
+    const width = annotation.width * canvas.width;
+    const height = annotation.height * canvas.height;
+
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(x, y, width, height);
+  }
+}
+
+function downloadAnnotations() {
+  for (let i = 0; i < annotations.length; i++) {
+    const imageAnnotations = annotations[i];
+    const image = images[i];
+    const darknetFormat = [];
+
+    for (let j = 0; j < imageAnnotations.length; j++) {
+      const annotation = imageAnnotations[j];
+      const x = annotation.x + annotation.width / 2;
+      const y = annotation.y + annotation.height / 2;
+      const width = annotation.width;
+      const height = annotation.height;
+
+      darknetFormat.push(`0 ${x} ${y} ${width} ${height}`);
+    }
+
+    const imageBlob = dataURItoBlob(image.src);
+    const darknetText = darknetFormat.join('\n');
+
+    const timestamp = new Date().toISOString().replace(/[-T:.Z]/g, ''); // Current date and time stamp
+    const imageFilename = `${timestamp}.png`;
+    const annotationFilename = `${timestamp}.txt`;
+
+    zip.file(imageFilename, imageBlob);
+    zip.file(annotationFilename, darknetText);
+  }
+
+  // Generate and download the ZIP file
+  zip.generateAsync({ type: "blob" })
+    .then(function (content) {
+      saveAs(content, "annotations.zip");
+    });
+}
+
+// Function to convert data URI to Blob
+function dataURItoBlob(dataURI) {
+  const byteString = atob(dataURI.split(',')[1]);
+  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type: mimeString });
+}
+
+// Next and previous image buttons
+document.getElementById('next-image').addEventListener('click', () => {
+  if (currentImageIndex < images.length - 1) {
+    setCurrentImage(currentImageIndex + 1);
+  }
+});
+
+document.getElementById('prev-image').addEventListener('click', () => {
+  if (currentImageIndex > 0) {
+    setCurrentImage(currentImageIndex - 1);
+  }
+});
--- login.js
+++ login.js
@@ -1,15 +1,93 @@
-document.getElementById('login-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission
-  
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    if (username === 'admin' && password === 'admin') {
-      // Correct login, redirect to the annotation page
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
+  import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyC35TZ5fCp_duJkk-CJZhFBWj5vOJUCew4",
+
+    authDomain: "parkar-2ab66.firebaseapp.com",
+
+    projectId: "parkar-2ab66",
+
+    storageBucket: "parkar-2ab66.appspot.com",
+
+    messagingSenderId: "636129511176",
+
+    appId: "1:636129511176:web:2f8d91902e81e90c7c4ebd",
+
+    measurementId: "G-97MRMXF5ND"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const submitButton = document.getElementById("submit");
+const signupButton = document.getElementById("sign-up");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+const createacct = document.getElementById("create-acct")
+
+
+
+const returnBtn = document.getElementById("return-btn");
+
+var email, password
+
+
+
+submitButton.addEventListener("click", function() {
+  email = emailInput.value;
+  console.log(email);
+  password = passwordInput.value;
+  console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+    //   REMOVE THIS IN FINAL PRODUCT SECUIRTY RISK
+      console.log(user.email)
+      console.log(user.password)
+    //log the email and passowrd returned by firebase
+      window.alert("Success! Welcome back!");
       window.location.href = 'annotation_tool.html'; // Replace with the actual URL of your annotation page
-    } else {
-      // Incorrect login, show alert
-      alert('Login information is invalid. Please try again.');
-    }
-  });
+
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error occurred. Try again. For Account Creation Please Contact Parkar Support");
+      window.alert("Error occurred. Try again. For Account Creation Please Contact Parkar Support");
+
+    });
+});
+signupButton.addEventListener("click", function() {
+    main.style.display = "none";
+    createacct.style.display = "block";
+});
+returnBtn.addEventListener("click", function() {
+    main.style.display = "block";
+    createacct.style.display = "none";
+});
   
+
